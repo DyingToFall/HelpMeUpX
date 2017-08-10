36e1de60860df043c497f270fa1a00fb52939bc3
DyingToFall <shonzero@gmail.com>
2017-08-09 18:12:35 -0700

    Correct Commit.

--- app/src/main/java/com/dyingtofall/helpmeupx/MainActivity.java
+++ app/src/main/java/com/dyingtofall/helpmeupx/MainActivity.java
@@ -2,16 +2,30 @@ package com.dyingtofall.helpmeupx;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+
+    Button sendBtn;
+    String phoneNo;
+    String message;
+    private SMS msms;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sendBtn = (Button) findViewById(R.id.button1);
+        msms = new SMS();
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                msms.sendSMSMessage();
+            }
+        });
        // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         //setSupportActionBar(toolbar);
 

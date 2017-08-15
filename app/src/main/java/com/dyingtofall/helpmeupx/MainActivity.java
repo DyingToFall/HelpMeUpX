270ccc0c09a2eb5170d4e2959791fa85b3b1b49e
Matt Stuart <matt80112@gmail.com>
2017-08-14 18:51:22 -0700

    Added stuff

--- app/src/main/java/com/dyingtofall/helpmeupx/MainActivity.java
+++ app/src/main/java/com/dyingtofall/helpmeupx/MainActivity.java
@@ -5,29 +5,38 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.SmsManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.content.DialogInterface;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity
 {
 
     Button sendBtn;
-    String phoneNo;
-    String message;
-    private SMS msms;
+
+    //String phoneNo;
+    //String message;
+    //private SMS msms;
+    protected SMS smsSend;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         sendBtn = (Button) findViewById(R.id.button1);
-        msms = new SMS();
+        smsSend = new SMS(this);
         sendBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view)
             {
-                sendSMSMessage();
+                //SMS smsSend = new SMS();
+                smsSend.sendSMSMessage();
+
                 //msms.sendSMSMessage();    //potential use for when using SMS class
 
             }
@@ -38,7 +47,7 @@ public class MainActivity extends AppCompatActivity
 
     }
 
-    protected void sendSMSMessage()  //needs to be moevd back to SMS class
+   /* protected void sendSMSMessage()  //needs to be moevd back to SMS class
     {
         phoneNo = "15413908735";
         message = "Help I have fallen and can't get up";
@@ -48,9 +57,9 @@ public class MainActivity extends AppCompatActivity
         SmsManager sms = SmsManager.getDefault();
         sms.sendTextMessage(phoneNo, null, message, pi, null);
 
-    }
+    }*/
 
-   /* @Override
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -58,17 +67,30 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+      /*  if (id == R.id.action_settings) {
             return true;
+        }*/
+        switch(item.getItemId())
+        {
+            case R.id.bluetooth:
+                BluetoothDialogFrag blueToothDialog = new BluetoothDialogFrag();
+                blueToothDialog.show(getFragmentManager(), "bluetooth dialog");
+                break;
+
+            default:
+                return super.onOptionsItemSelected(item);
         }
 
-        return super.onOptionsItemSelected(item);
-    }*/
+        return true;
+    }
+
+
 }
--- app/src/main/java/com/dyingtofall/helpmeupx/SMS.java
+++ app/src/main/java/com/dyingtofall/helpmeupx/SMS.java
@@ -1,14 +1,26 @@
 package com.dyingtofall.helpmeupx;
 
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.telephony.SmsManager;
 import android.widget.Button;
 
 
 public class SMS extends Activity
 {
-    Button sendBtn;
+    //Button sendBtn;
     String phoneNo;
     String message;
+    public MainActivity mainActivity;
+    private Context context;
+
+    public SMS(MainActivity mainActivity)
+    {
+        super();
+        this.mainActivity = mainActivity;
+    }
 
 
 
@@ -24,19 +36,18 @@ public class SMS extends Activity
                 sendSMSMessage();
             }
         });
-    }
-
-
-    protected void sendSMSMessage()  //commented out to test in MainActivity
+    }*/
+    //this.mainActivity = mainActivity;
+    //this.context = context;
+    protected void sendSMSMessage()
     {
-        phoneNo = "15413908735";
+        //phoneNo = "15413908735";
+        phoneNo = "16199525946";
         message = "Help I have fallen and can't get up";
-
-        PendingIntent pi = PendingIntent.getActivity(this, 0,
-                new Intent(this, SMS.class), 0);
+        PendingIntent pi = PendingIntent.getActivity(mainActivity, 0, new Intent(this, SMS.class), 0);   // needs to be changed back to SMS class when moved
         SmsManager sms = SmsManager.getDefault();
         sms.sendTextMessage(phoneNo, null, message, pi, null);
 
     }
-*/
+
 }
